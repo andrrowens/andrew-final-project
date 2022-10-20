6d06f7ec9cebc01f2158c46fa7568ab7711d35c5
Andrew Owens <andrrowens@gmail.com>
2022-10-19 18:54:41 -0600

    Updates to index.html, style.css, and script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Please pardon our dust, we are under construction. Expected completion Feb. 2023";
 
 document.querySelector("body").appendChild(h2);
