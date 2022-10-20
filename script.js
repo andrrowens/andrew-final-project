d7727208505c4b930fcb40f3dbb06b0da5e8836a
Andrew Owens <andrrowens@gmail.com>
2022-10-19 19:26:13 -0600

    added squawk event listener on bird

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const h2 = document.createElement("h2");
 h2.textContent = "Please pardon our dust, we are under construction. Expected completion Feb. 2023";
 
 document.querySelector("body").appendChild(h2);
+
+const img = bird
+
+const input = document.getElementById('bird');
+input.addEventListener('click', function() {
+  alert('Squawk!');
+});
